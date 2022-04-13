810ad8830bd5111f6d7d14b3dc879adf092e9bbf
Andrea <marchioandrea02@gmail.com>
2022-04-13 21:27:46 +0200

    Modificati File

--- src/main/java/com/progetto/progetto/MainController.java
+++ src/main/java/com/progetto/progetto/MainController.java
@@ -3,7 +3,7 @@ package com.progetto.progetto;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
