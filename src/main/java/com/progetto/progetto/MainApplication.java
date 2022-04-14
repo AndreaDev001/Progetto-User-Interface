050c54a26025c06675f00a9b8972e7e6fd132747
Andrea <marchioandrea02@gmail.com>
2022-04-14 10:15:47 +0200

    Aggiunti e modificati File

--- src/main/java/com/progetto/progetto/MainApplication.java
+++ src/main/java/com/progetto/progetto/MainApplication.java
@@ -1,9 +1,13 @@
 package com.progetto.progetto;
 
+import info.movito.themoviedbapi.TmdbMovies;
+import info.movito.themoviedbapi.model.ArtworkType;
+import info.movito.themoviedbapi.model.MovieDb;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import info.movito.themoviedbapi.TmdbApi;
 
 import java.io.IOException;
 
@@ -11,8 +15,12 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 640,480);
+        TmdbMovies movies = new TmdbApi("3837271101e801680438310f38a3feff").getMovies();
+        MovieDb movie = movies.getMovie(5353,"en");
+        movie.getImages(ArtworkType.POSTER);
+        System.out.println(movie.getTitle());
+        stage.setTitle("Main Application");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/progetto/progetto/model/sql/MySQL.java
@@ -0,0 +1,52 @@
+package com.progetto.progetto.model.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQL
+{
+    private final String host;
+    private final String database;
+    private final String username;
+    private final String password;
+    private final String port;
+    private final boolean useSSL;
+    private Connection connection;
+
+    public MySQL(String host,String database,String username,String password,String port,boolean useSSL)
+    {
+        this.host = host;
+        this.database = database;
+        this.username = username;
+        this.password = password;
+        this.port = port;
+        this.useSSL = useSSL;
+    }
+    public void Connect()
+    {
+        String usesSSL = useSSL ? "?useSSL=true" : "?useSSL=false";
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database + usesSSL,username,password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void Disconnect()
+    {
+        try {
+            connection.close();
+            connection = null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean isConnected() {return connection != null;}
+    public final String getHost() {return host;}
+    public final String getDatabase() {return database;}
+    public final String getUsername() {return username;}
+    public final String getPassword() {return password;}
+    public final String getPort() {return port;}
+    public boolean usesSSL() {return useSSL;}
+    public Connection getConnection() {return connection;}
+}
