ec8373280a553780e8f2a42a137044a7b467cd93
Andrea <marchioandrea02@gmail.com>
2022-04-15 13:41:51 +0200

    Aggiunti e Modificati File

--- src/main/java/com/progetto/progetto/view/SceneHandler.java
+++ src/main/java/com/progetto/progetto/view/SceneHandler.java
@@ -2,12 +2,16 @@ package com.progetto.progetto.view;
 
 import com.progetto.progetto.MainApplication;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class SceneHandler {
 
@@ -37,6 +41,7 @@ public class SceneHandler {
         try {
             return fxmlLoader.load();
         } catch (IOException e) {
+            e.printStackTrace();
             createAlertMessage("Error","Something went wrong while loading a scene!", Alert.AlertType.ERROR);
         }
         return null;
@@ -50,22 +55,46 @@ public class SceneHandler {
     }
 
     //---------------------------SCENES------------------------------//
-
-    public void loadLoginScene()
+    public void loadMainScene()
     {
+        stage.hide();
         Parent root = loadRootFromFXML("MainView.fxml");
         if(root == null)
             return;
         if(this.scene == null)
             this.scene = new Scene(root);
+        this.scene.getStylesheets().clear();
         this.scene.setRoot(root);
-        stage.setTitle("Login Scene");
+        stage.setWidth(1280);
+        stage.setHeight(720);
         stage.setMinWidth(640);
         stage.setMinHeight(480);
-        stage.setWidth(stage.getMinWidth());
-        stage.setHeight(stage.getMinWidth());
+        stage.setResizable(true);
+        stage.setTitle("Main Scene");
+        centerStage(stage.getWidth(),stage.getHeight());
+        stage.show();
     }
-
-
-
+    public void loadLoginScene()
+    {
+        Parent root = loadRootFromFXML("LoginView.fxml");
+        if(root == null)
+            return;
+        if(this.scene == null)
+            this.scene = new Scene(root);
+        this.scene.getStylesheets().clear();
+        this.scene.setRoot(root);
+        stage.setMinWidth(500);
+        stage.setMinHeight(300);
+        stage.setTitle("Login View");
+        stage.setWidth(stage.getMinWidth());
+        stage.setHeight(stage.getMinHeight());
+        stage.setResizable(false);
+        centerStage(stage.getWidth(),stage.getHeight());
+    }
+    public void centerStage(double width,double height)
+    {
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setX((screenBounds.getWidth() - width) / 2);
+        stage.setY((screenBounds.getHeight() - height) / 2);
+    }
 }
